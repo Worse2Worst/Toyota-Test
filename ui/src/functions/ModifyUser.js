28e87ce97a4040a485e3baca2beb6bb3f3ca8559
Worse2Worst <wasuwach@hotmail.com>
2022-08-13 21:48:35 +0900

    dropdown fix

--- ui/src/functions/ModifyUser.js
+++ ui/src/functions/ModifyUser.js
@@ -1,26 +1,46 @@
 import '../App.css';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Dropdown from 'react-bootstrap/Dropdown';
 
+
+const baseURL = "http://127.0.0.1:5000/v1/users";
+
 function ModifyUser() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(baseURL + "?id=all", {
+      'method': 'GET',
+      headers: {
+        'Content-Type': 'applications/json'
+      }
+    })
+      .then(resp => resp.json())
+      .then(resp => setUsers(resp))
+      .catch(error => console.log(error))
+  }, [])
   return (
     <div className='app'>
     <div className='card'>
     <Card>
-    <Card.Header>Modify User Name</Card.Header>
+    <Card.Header>Modify User's Name</Card.Header>
     <Card.Body>
      {/*TODO, have to look into dropdown thing, change drop down menu too */}
     <Dropdown>
       <Dropdown.Toggle variant="light" id="dropdown-basic">
-        Please select id to modify the username
+        Please select id to modify the user's name
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+         {users.map(user => {
+          return (
+        // <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item>{user.id}</Dropdown.Item>
+          )
+         })}
       </Dropdown.Menu>
     </Dropdown>
 
