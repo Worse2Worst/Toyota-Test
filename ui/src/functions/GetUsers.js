b03094cf08dbb9571f360aae97235dd249f92230
Worse2Worst <wasuwach@hotmail.com>
2022-08-13 22:03:19 +0900

    Axios

--- ui/src/functions/GetUsers.js
+++ ui/src/functions/GetUsers.js
@@ -4,24 +4,42 @@ import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
 import React, { useState, useEffect } from 'react';
 import UsersList from './UsersList';
+import api from '../api/axios';
 
-
-
-const baseURL = "http://127.0.0.1:5000/v1/users?id=all";
 
 function GetUsers() {
   const [users, setUsers] = useState([]);
 
+  // useEffect(() => {
+  //   fetch(baseURL, {
+  //     'method': 'GET',
+  //     headers: {
+  //       'Content-Type': 'applications/json'
+  //     }
+  //   })
+  //     .then(resp => resp.json())
+  //     .then(resp => setUsers(resp))
+  //     .catch(error => console.log(error))
+  // }, [])
+
   useEffect(() => {
-    fetch(baseURL, {
-      'method': 'GET',
-      headers: {
-        'Content-Type': 'applications/json'
+    const fetchUsers = async () => {
+      try {
+        const response = await api.get('/users?id=all');
+        setUsers(response.data);
+      } catch (err) {
+        if (err.response) {
+          // Not in the 200 response range 
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
       }
-    })
-      .then(resp => resp.json())
-      .then(resp => setUsers(resp))
-      .catch(error => console.log(error))
+    }
+
+    fetchUsers();
   }, [])
 
   return (
