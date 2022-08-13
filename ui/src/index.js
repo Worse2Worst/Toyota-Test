dffbb22388bcdf39b3cb71f0bb02cf8acc96e962
Worse2Worst <wasuwach@hotmail.com>
2022-08-13 20:21:41 +0900

    Revert "Plural"
    This reverts commit 703471c40018552c9c29e4e4623b37951bcb878c.

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import AddUser from './functions/AddUser';
 import DeleteUser from './functions/DeleteUser';
 import ModifyUser from './functions/ModifyUser';
-import GetUsers from './functions/GetUsers';
+import GetUser from './functions/GetUser';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -19,7 +19,7 @@ root.render(
     <h1>Home</h1>
     </div>
     <AddUser />
-    <GetUsers />
+    <GetUser />
     <ModifyUser />
     <DeleteUser />
   </React.StrictMode>
