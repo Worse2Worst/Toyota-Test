9370847b4d8b17161e3e697186f245ccef159467
Worse2Worst <wasuwach@hotmail.com>
2022-08-14 22:39:52 +0900

    Renamed folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import AddUser from './functions/AddUser';
-import DeleteUser from './functions/DeleteUser';
-import ModifyUser from './functions/ModifyUser';
-import GetUsers from './functions/GetUsers';
+import AddUser from './forms/AddUser';
+import DeleteUser from './forms/DeleteUser';
+import ModifyUser from './forms/ModifyUser';
+import GetUsers from './forms/GetUsers';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
