08dc726b992f6cc5782a187e49446d1ba6e61572
Worse2Worst <wasuwach@hotmail.com>
2022-08-14 11:17:39 +0900

    Catching errors

--- ui/src/functions/AddUser.js
+++ ui/src/functions/AddUser.js
@@ -7,12 +7,18 @@ import api from '../api/axios';
 
 
 function AddUser() {
-  
+
   const [users, setUsers] = useState([]);
+  const [userName, setUserName] = useState([]);
+  const [email, setEmail] = useState([]);
+
+  const onNameInput = ({target:{value}}) => setUserName(value);
+  const onEmailInput = ({target:{value}}) => setEmail(value);
 
   const fetchUsers = async () => {
     try {
-      const response = await api.get('/users?id=all');
+      const params = new URLSearchParams([['id', 'all']]);
+      const response = await api.get('/users', { params });
       setUsers(response.data);
     } catch (err) {
       if (err.response) {
@@ -30,16 +36,39 @@ function AddUser() {
     fetchUsers();
   }, [])
 
-  let state = {
-    name: "",
-    email: ""
-  };
-
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(state.name);
-    console.log(state.email);
+    console.log(userName);
+    console.log(email);
+    // Calling the API
+    const jsonBody = {
+      'name': userName,
+      'email': email
+    }
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      params: {
+        'type': 'add'
+      }
+    }
+    try{
+      const response = await api.post('/users', jsonBody, config);
+    } catch (err) {
+        if (err.response) {
+          // Not in the 200 response range 
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+    }
   }
+
+
 
   return (
     <div className='app'>
@@ -47,10 +76,11 @@ function AddUser() {
     <Card>
     <Card.Header>Add User</Card.Header>
     <Card.Body>
-      <Form>
+      <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formAddName">
           <Form.Control 
-            type="text" 
+            type="text"
+            onChange={onNameInput}
             placeholder="Please input your name..." 
             />
         </Form.Group>
@@ -58,6 +88,7 @@ function AddUser() {
         <Form.Group className="mb-3" controlId="formAddEmail">
           <Form.Control 
           type="email" 
+          onChange={onEmailInput}
           placeholder="Please input your email..."
           />
         </Form.Group>
--- ui/src/functions/ModifyUser.js
+++ ui/src/functions/ModifyUser.js
@@ -13,17 +13,18 @@ function ModifyUser() {
 
   const fetchUsers = async () => {
     try {
-      const response = await api.get('/users?id=all');
+      const params = new URLSearchParams([['id', 'all']]);
+      const response = await api.get('/users', { params });
       setUsers(response.data);
     } catch (err) {
-      if (err.response) {
-        // Not in the 200 response range 
-        console.log(err.response.data);
-        console.log(err.response.status);
-        console.log(err.response.headers);
-      } else {
-        console.log(`Error: ${err.message}`);
-      }
+        if (err.response) {
+          // Not in the 200 response range 
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
     }
   }
 
