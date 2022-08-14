ff6254bea566b272d9834656f457472aed6e033b
Worse2Worst <wasuwach@hotmail.com>
2022-08-14 15:15:46 +0900

    Revert "Revert "Prepare for deploy""
    This reverts commit 0811bda7ee3b322d760cb91eed2a45d367d44e46.

--- ui/src/api/axios.js
+++ ui/src/api/axios.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+
+let development = process.env.NODE_ENV !== 'production'
+let url = development ? 'http://127.0.0.1:5000/v1/' : 'https://toyota-fullstack-test.herokuapp.com/v1/'
+
 export default axios.create({
-    baseURL: 'http://127.0.0.1:5000/v1/'
+    baseURL: url
 });
